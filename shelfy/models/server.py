bfe4d11aa95120cd5844debed2e6047e846fe38d
tphinkle <tphinkle@gmail.com>
2018-01-14 13:42:54 -0800

    update

--- shelfy/models/server.py
+++ shelfy/models/server.py
@@ -86,7 +86,7 @@ def get_raw_image_path_from_submission_id(submission_id):
     file_name = [file_name for file_name in os.listdir(file_directory) \
      if os.path.isfile(os.path.join(file_directory, file_name))][0]
 
-     print('FILE_NAME', file_name)
+    print('FILE_NAME', file_name)
 
 
 
