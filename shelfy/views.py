5d86f12a91096b2c184b3859d39fc8d88018099b
tphinkle <tphinkle@gmail.com>
2018-01-14 17:00:15 -0800

    update

--- shelfy/views.py
+++ shelfy/views.py
@@ -21,9 +21,9 @@ def uploads(submission_id):
     raw_img_file_path = server.get_raw_image_path_from_submission_id(submission_id)
 
 
-    print('raw_image_file_path!', raw_image_file_path)
+    print('raw_img_file_path!', raw_img_file_path)
 
-    return flask.render_template('submission.html', raw_image_file_path = raw_img_file_path)
+    return flask.render_template('submission.html', rawimgfilepath = raw_img_file_path)
 
 
 @views.route('/', methods=['GET', 'POST'])
