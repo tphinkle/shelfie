64ddb287eb1a72153ee9c5f2ba54b30aa2029516
tphinkle <tphinkle@gmail.com>
2018-01-14 17:52:02 -0800

    update

--- shelfy/views.py
+++ shelfy/views.py
@@ -77,7 +77,7 @@ def index():
             books = scraper.full_pipeline(raw_file_path)
 
             # Save the processed image
-            proc_file_path = server.get_proc_image_path_from_submission_id(submission_id)
+            proc_file_path = server.get_processed_image_path_from_submission_id(submission_id)
             book_functions.generate_processed_image(books, raw_file_path, save_path = proc_file_path)
 
             # Pickle and save the books
