1fd8adba7fe2c5843f10b8878234ec6735795106
tphinkle <tphinkle@gmail.com>
2018-01-14 16:22:34 -0800

    update

--- shelfy/models/server.py
+++ shelfy/models/server.py
@@ -156,4 +156,4 @@ def save_book_info(books, submission_id):
 
     # Dump the info to a json file
     with open(info_directory + '/info.json', 'wb') as file_handle:
-        json.dump([book.book_info for book in books], file_handle)
+        json.dumps([book.book_info for book in books], file_handle)
