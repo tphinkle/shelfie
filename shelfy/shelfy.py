73372d192612196f95eadf569c9adea19f34e210
tphinkle <tphinkle@gmail.com>
2018-01-16 22:25:03 -0800

    update

--- shelfy/shelfy.py
+++ shelfy/shelfy.py
@@ -1,2 +1,63 @@
+'''
+INCLUDE THIS IF RUNNING VIA
+flask run (or, through compile.sh)
+'''
+
+
+'''
 import shelfy
 app = shelfy.app
+
+
+'''
+
+
+
+
+'''
+ONLY INCLUDE THIS IF RUNNING VIA
+python shelfy.py
+'''
+
+# Imports
+import flask
+import os
+from werkzeug.utils import secure_filename    # Needed for image upload
+
+
+# Flask location
+
+
+# Base path for project
+SHELFY_BASE_PATH = os.path.dirname(__file__)
+
+
+
+# Configure app
+app = flask.Flask(__name__)
+
+
+
+
+
+app.config.from_object(__name__) # Load config from thsie file (permit_generator.py)
+
+app.config.update(dict(
+SECRET_KEY = 'key',
+USERNAME = 'admin',
+PASSWORD = 'default'
+))
+
+
+
+
+
+# Register view blueprints
+import shelfy.views
+
+
+app.register_blueprint(shelfy.views.views)
+
+
+if __name__ == 'main':
+    app.run(host='0.0.0.0', port=80)
