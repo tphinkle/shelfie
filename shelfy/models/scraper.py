d229d03e9fe960180cd344c14766482f23efc828
tphinkle <tphinkle@gmail.com>
2018-01-19 14:56:34 -0800

    update

--- shelfy/models/scraper.py
+++ shelfy/models/scraper.py
@@ -768,8 +768,10 @@ def get_prices_from_amazon_products(isbn, amazon):
         # Get next sales url
         sales_url = get_next_sales_url_from_sales_page_soup(sales_page_soup, base_url)
 
-    book_price = AmazonPrice(total_prices, total_shipping_prices, total_qualities)
 
+    print('creating price')
+    book_price = AmazonPrice(total_prices, total_shipping_prices, total_qualities)
+    print('done creating price')
 
     return book_price
 
