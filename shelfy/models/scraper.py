9442fd0b716b631915a05dade1f3936afaf4a4ea
tphinkle <tphinkle@gmail.com>
2018-01-18 17:24:26 -0800

    update

--- shelfy/models/scraper.py
+++ shelfy/models/scraper.py
@@ -610,9 +610,9 @@ def get_book_info_amazon_products(isbn, amazon):
     # Get the book info
     book_info = {}
 
-    book_info['title'] = soup.ItemLookupResponse.Items.Item.ItemAttributes.Title
-    book_info['author'] = soup.ItemLookupResponse.Items.Item.ItemAttributes.Author
-    book_info['publisher'] = soup.ItemLookupResponse.Items.Item.ItemAttributes.Publisher
+    book_info['title'] = soup.ItemLookupResponse.Items.Item.ItemAttributes.Title.contents
+    book_info['author'] = soup.ItemLookupResponse.Items.Item.ItemAttributes.Author.contents
+    book_info['publisher'] = soup.ItemLookupResponse.Items.Item.ItemAttributes.Publisher.contents
 
     return book_info
 
