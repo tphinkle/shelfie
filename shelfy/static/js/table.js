dcdc5f965960e63641b80e2705c633bd08824228
tphinkle <tphinkle@gmail.com>
2018-01-20 10:43:16 -0800

    update

--- shelfy/static/js/table.js
+++ shelfy/static/js/table.js
@@ -1,14 +1,10 @@
-$(document).ready(function () {
-    console.log('loaded');
-    //iterate through each row in the table
-    $('tr').each(function () {
-        //the value of sum needs to be reset for each row, so it has to be set inside the row loop
-        var sum = 0
-        //find the combat elements in the current row and sum it
-        $(this).find('td').each(function () {
-            var value = $(this).text();
-            sum += Number(value.replace(/[^0-9/.-]+/g,""));
-        });
-      });
-      $('.total-price', this).html(sum);
+
+
+
+// Total updating function
+
+$(document).ready(function() {
+    $('input[type=checkbox][name=sell-checkbox]').change(function() {
+        console.log($(this).value());
+    });
 });
