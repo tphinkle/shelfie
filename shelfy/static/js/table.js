f8b32321f2477efbe0f8d0cc26239c5bf5efdbc9
tphinkle <tphinkle@gmail.com>
2018-01-20 15:26:56 -0800

    update

--- shelfy/static/js/table.js
+++ shelfy/static/js/table.js
@@ -1,47 +1,25 @@
-console.log('asdf');
-
-// Total updating function
-
-function sum_table() {
-  console.log('123');
-
-
-
-
+$(document).ready(function() {
+  $("input[type=checkbox]").change(function(){
+    recalculate();
+  });
 }
 
 
 
-$(document).ready(function() {
-    console.log('Trying to do something');
-    $('.form-check-input').each(
-      $(this).change(function() {
+// recalculate the table sum
+function recalculate(){
+    var sum = 0;
 
+    $("input[type=checkbox]:checked").each(function(){
+      sum += price_to_float($(this).attr("rel"));
+    });
 
-        // Loop over all of table
-        var total = 0;
-        var price;
-        var number;
-
-        $('#table table-hover > tbody  > tr:not(:first)').each( function(){
-            console.log('zws');
-
-            price = $('this > #price').value();
-
-            console.log(price);
-
-            number = Number(price.replace(/[^0-9\.-]+/g,""));
-            total = total + number;
-          });
+    alert(sum);
+}
 
 
 
-        // Format the price
-        total.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-        console.log(total);
-
-
-        // Set value of the label to the total calculated price
-        $('#total-label').val(total);
-    }));
-});
+// Convert price to floating point number
+function price_to_float(price) {
+  return price.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')
+}
