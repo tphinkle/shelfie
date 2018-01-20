f90b29efb014b13953fd7aa5559a8767ed804604
tphinkle <tphinkle@gmail.com>
2018-01-20 14:23:02 -0800

    update

--- shelfy/static/js/table.js
+++ shelfy/static/js/table.js
@@ -4,8 +4,25 @@ console.log('asdf');
 
 function sum_table() {
   console.log('123');
-  //var price = $(this).val()
-  //var number = Number(price.replace(/[^0-9\.-]+/g,""));
+
+  var total = 0;
+  var price;
+  var number;
+
+  // Loop over table
+  $('.price_row').each(
+      price = $(this).val();
+      number = Number(price.replace(/[^0-9\.-]+/g,""));
+      total = total + number;
+    )
+
+
+  // Format the price
+  total.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+  console.log(total);
+  return total;
+
+
 }
 
 
