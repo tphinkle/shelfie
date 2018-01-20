c057db4f644cf260f1f2942e063fe5712cb6eefd
tphinkle <tphinkle@gmail.com>
2018-01-20 15:20:03 -0800

    update

--- shelfy/static/js/table.js
+++ shelfy/static/js/table.js
@@ -5,27 +5,7 @@ console.log('asdf');
 function sum_table() {
   console.log('123');
 
-  var total = 0;
-  var price;
-  var number;
 
-  $('#table table-hover > tbody  > tr:not(:first)').each( function(){
-      console.log('zws');
-
-      price = $('this > #price').value();
-
-      console.log(price);
-
-      number = Number(price.replace(/[^0-9\.-]+/g,""));
-      total = total + number;
-    });
-
-
-
-  // Format the price
-  total.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-  console.log(total);
-  return total;
 
 
 }
@@ -36,8 +16,32 @@ $(document).ready(function() {
     console.log('Trying to do something');
     $('.form-check-input').each(
       $(this).change(function() {
-        console.log('asdf');
-        sum_table();
-        $('#total-label').val(sum_table());
+
+
+        // Loop over all of table
+        var total = 0;
+        var price;
+        var number;
+
+        $('#table table-hover > tbody  > tr:not(:first)').each( function(){
+            console.log('zws');
+
+            price = $('this > #price').value();
+
+            console.log(price);
+
+            number = Number(price.replace(/[^0-9\.-]+/g,""));
+            total = total + number;
+          });
+
+
+
+        // Format the price
+        total.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+        console.log(total);
+
+
+        // Set value of the label to the total calculated price
+        $('#total-label').val(total);
     }));
 });
