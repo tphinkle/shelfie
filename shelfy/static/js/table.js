4304373ac81e856951b5257ad22d835b8564ee41
tphinkle <tphinkle@gmail.com>
2018-01-20 14:15:06 -0800

    update

--- shelfy/static/js/table.js
+++ shelfy/static/js/table.js
@@ -2,6 +2,11 @@ console.log('asdf');
 
 // Total updating function
 
+function sum_table() {
+  return 3;
+  //var price = $(this).val()
+  //var number = Number(price.replace(/[^0-9\.-]+/g,""));
+}
 
 
 
@@ -10,5 +15,6 @@ $(document).ready(function() {
     $('.form-check-input').each(
       $(this).change(function() {
         console.log('asdf');
+        $('#total-label').val(sum_table());
     }));
 });
