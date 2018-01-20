0da69482d9427f372eb69298aee0af26fc178a82
tphinkle <tphinkle@gmail.com>
2018-01-20 10:47:59 -0800

    update

--- shelfy/static/js/table.js
+++ shelfy/static/js/table.js
@@ -4,6 +4,7 @@
 // Total updating function
 
 $(document).ready(function() {
+    console.log('Trying to do something');
     $('input[type=checkbox][name=sell-checkbox]').change(function() {
         console.log($(this).value());
     });
