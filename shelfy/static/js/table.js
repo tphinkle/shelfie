05a5425584d1e7a784b3f8dbbad79900e7c9b159
tphinkle <tphinkle@gmail.com>
2018-01-20 16:33:15 -0800

    update

--- shelfy/static/js/table.js
+++ shelfy/static/js/table.js
@@ -10,11 +10,17 @@ function format_price(price) {
 }
 
 // recalculate the table sum
-function recalculate_total_price(obj){
+function recalculate_total_price(){
   var total_price = 0;
   $('#cost_table > tbody  > tr').not(":first").each(function() {
-    //if $(this).children('td[name="checkbox"]')
-    total_price += price_to_float($(this).children('td[name="price_row"]').text());
+
+    // Check if item is selected
+    //if ($(this).children('td[name="checkbox"]')) {
+
+      // If selected, update the total price
+      total_price += price_to_float($(this).children('td[name="price_row"]').text());
+  //}
+
   });
 
  return total_price;
@@ -26,11 +32,12 @@ function recalculate_total_price(obj){
 $(document).ready(function() {
 
   console.log('page loaded');
+  recalculate_total_price();
 
   // Loop over all checkboxes
   $('.form-check-input').each(function(i, obj) {
     $(this).change(function(){
-      var total_price = recalculate_total_price($(this));
+      var total_price = recalculate_total_price();
       var formatted_total_price = format_price(total_price);
       $('#total-label').text(formatted_total_price);
     });
