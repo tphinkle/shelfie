3d1a5210a726442ea6aa89a69ef2f1698c3a3192
tphinkle <tphinkle@gmail.com>
2018-01-23 20:02:42 -0800

    update

--- shelfy/__init__.py
+++ shelfy/__init__.py
@@ -1,18 +1,1 @@
-# Imports
-import os
 
-import flask
-
-
-# Base path for project
-SHELFY_BASE_PATH = os.path.dirname(__file__)
-
-
-# Configure app
-app = flask.Flask(__name__)
-app.config.from_object(__name__) # Load config from thsie file (permit_generator.py)
-app.config.update(dict(
-SECRET_KEY = 'key',
-USERNAME = 'admin',
-PASSWORD = 'default'
-))
--- shelfy/main.py
+++ shelfy/main.py
@@ -1,26 +1,31 @@
-'''
-INCLUDE THIS IF RUNNING VIA
-flask run (or, through compile.sh)
-'''
-
-
-
 # Imports
 import flask
-import __init__#app, SHELFY_BASE_PATH
 import os
 from werkzeug.utils import secure_filename    # Needed for image upload
 import sys
+
+# Base path for project
+SHELFY_BASE_PATH = os.path.dirname(__file__)
+
+
+# Configure app
+app = flask.Flask(__name__)
+app.config.from_object(__name__) # Load config from thsie file (permit_generator.py)
+app.config.update(dict(
+SECRET_KEY = 'key',
+USERNAME = 'admin',
+PASSWORD = 'default'
+))
 
 
 
 
 # Register view blueprints
-sys.path.append(__init__.SHELFY_BASE_PATH)
+sys.path.append(SHELFY_BASE_PATH)
 import views
-__init__.app.register_blueprint(views.views)
+app.register_blueprint(views.views)
 
 
 
 if __name__ == '__main__':
-    __init__.app.run()
+    app.run()
