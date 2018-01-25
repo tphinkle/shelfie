baa4deac396549b09a4938674582cf7026f3befd
tphinkle <tphinkle@gmail.com>
2018-01-25 15:53:39 -0800

    update

--- shelfy/models/sql_handle.py
+++ shelfy/models/sql_handle.py
@@ -37,7 +37,7 @@ class SQLHandle(object):
             SQLHandle.connect_to_database()
 
         SQLHandle.cursor.execute(command)
-        results = SQLHandle.cursor.fetchall()
+        results = SQLHandle.cursor.fetchall()[0]
 
         return results
 
