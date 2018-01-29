08e615d146dc1d20c17265da73e1e64ce128ad4f
tphinkle <tphinkle@gmail.com>
2018-01-28 17:22:59 -0800

    update

--- shelfy/static/js/bookshelf.js
+++ shelfy/static/js/bookshelf.js
@@ -5,13 +5,28 @@ ctx.fillStyle = "#FF0000";
 ctx.fillRect(0,0,150,75);
 
 
+get_submission_id = function() {
+  url = window.location.href;
+  console.log(url);
+}
 
+load_image = function() {
+  var canvas = $("#bookshelf");
+  var ctx = canvas.getContext("2d");
+
+  var bookshelf_img = new Image();
+
+  ctx.drawImage(bookshelf_img, 0, 0);
+
+  ctx.fillRect(0,500,500);
+
+  bookshelf_img.src = '/static/submissions/'
+
+}
 
 $(document).ready(function() {
 
-  var canvas = document.getElementById("bookshelf");
-  var ctx = canvas.getContext("2d");
-  ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0,0,150,75);
+  load_image();
+  get_submission_id();
 
 });
