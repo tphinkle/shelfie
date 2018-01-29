f12affa47376a5e2492e78fe75b7d0ca84d7a154
tphinkle <tphinkle@gmail.com>
2018-01-28 19:17:07 -0800

    update

--- shelfy/static/js/bookshelf.js
+++ shelfy/static/js/bookshelf.js
@@ -8,8 +8,9 @@ function get_submission_id() {
 }
 
 
-function register_new_title(book_center, book_name, book_author) {
-  console.log(book_center);
+function register_new_title(book_center_x, book_center_y, book_name, book_author) {
+  console.log(book_center_x);
+  console.log(book_center_y);
   console.log(book_name);
   console.log(book_author);
 }
