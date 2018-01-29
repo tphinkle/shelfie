efd649510fc8f2016da82913e44c4b65be4d5da7
tphinkle <tphinkle@gmail.com>
2018-01-28 17:05:21 -0800

    update

---
+++ shelfy/static/js/bookshelf.js
@@ -0,0 +1,17 @@
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);
+
+
+
+
+$(document).ready(function() {
+
+  var canvas = document.getElementById("bookshelf");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0,0,150,75);
+
+});
