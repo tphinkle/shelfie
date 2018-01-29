47465f37f37943b7014c6ba9eaf14f9a4e38602e
tphinkle <tphinkle@gmail.com>
2018-01-28 17:29:56 -0800

    update

--- shelfy/static/js/bookshelf.js
+++ shelfy/static/js/bookshelf.js
@@ -1,10 +1,3 @@
-
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,150,75);
-
-
 get_submission_id = function() {
   url = window.location.href;
   console.log(url);
