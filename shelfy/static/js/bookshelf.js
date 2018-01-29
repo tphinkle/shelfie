3847059778acef940b76ba584a4f88c50706d04a
tphinkle <tphinkle@gmail.com>
2018-01-28 18:48:25 -0800

    update

--- shelfy/static/js/bookshelf.js
+++ shelfy/static/js/bookshelf.js
@@ -11,16 +11,18 @@ function load_image(submission_id) {
 
   // Get canvas and canvas context object
   var canvas = document.getElementById('bookshelf');
+
+
   var ctx = canvas.getContext("2d");
 
   // Create and draw the image
-  var bookshelf_img = new Image();
+  var bookshelf_img = new Image(canvas.width, canvas.height);
 
   var img_path = '/static/submissions/' + submission_id + '/raw_img/raw_img.jpg';
 
   console.log(img_path);
 
-  bookshelf_img.onload = function(){
+  bookshelf_img.onload = function() {
     ctx.drawImage(bookshelf_img, bookshelf_img.width, bookshelf_img.height);
   }
 
