68b39d1c96d1b5731996c13e1d7bb1c9f22e529c
tphinkle <tphinkle@gmail.com>
2018-02-06 08:21:25 -0800

    update

--- shelfy/static/js/index.js
+++ shelfy/static/js/index.js
@@ -1,7 +1,7 @@
 
-
+console.log('123');
 // Toggle instructions when instructions button is pushed.
-$('#instructions_button').click(function() {
+$('#instructions_link').click(function() {
   console.log('asdf');
   $('#instructions').toggle();
 })
